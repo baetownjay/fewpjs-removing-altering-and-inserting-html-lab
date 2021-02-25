38e0868352ca7bad0a8c8a328720bd2c9e19c03c
Jason Herrera <baytownjason@yahoo.com>
2021-02-25 08:19:59 -0600

    commit tree 3ace1a5a004b1727ee6ad5b4c85cb3acd6d17b25

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your code here!
+// Write your code here!\
+let mainclass = document.getElementById('main')
+function deletemain (){
+    mainclass.remove()
+}
+deletemain()
+
+let newHeader = document.querySelector("#victory")
+
+
+
+
+
+// //create DOM element
+// let element = document.createElement('div');
+
+// //add element to node
+// document.body.appendChild(element)
+
+// //update element
+// let ul = document.createElement('ul')
+
+// for (i = 0; i<3; i++){
+//     let li = document.createElement('li')
+//     li.innerHTML = (i+1).toString()
+//     ul.appendChild(li)
+// }
+
+// element.appendChild(ul)
